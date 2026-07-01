ee8d3413244639e734018dccf65133648847e279
mahnoor451 <mahnoorishaq83@gmail.cm>
2026-07-01 18:41:27 +0500

    Fixed the image direction

--- components/header.jsx
+++ components/header.jsx
@@ -4,7 +4,7 @@ return (
     <>
     <header className="header">
          <h1 className="header__h1">Meme Generator</h1>
-        <img src="troll-face.png" className="header__img"/>
+        <img src="/troll-face.png" className="header__img"/>
     </header>
     </>
 )
